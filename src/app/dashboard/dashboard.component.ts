29b96f3cdf71ef64c0767d97509a9894cfcaeb32
Pavithra Muniraju <pavithra.muniraju@ust.com>
2025-07-16 10:18:59 +0530

    dashbaord count and results updated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,20 +7,27 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent {
   @Input() list:any = [];
+  query:any = '';
 
-  uniqueKnowledgeAreas:any = [];
+  uniqueKnowledgeAreasWithCount:any = [];
   constructor(private cdr: ChangeDetectorRef,private router: Router,) {}
   
   ngOnInit() {
-    console.log(this.list);
+    this.query = sessionStorage.getItem('query');
     this.cdr.detectChanges();
-
-    this.uniqueKnowledgeAreas  = [...new Set(this.list.map((item:any) => item.design_group))];
-    console.log(this.uniqueKnowledgeAreas); 
+    this.getUniqueKnowledgeArea();
   }
 
   getAllUniqueKnowledgeAreaItems(ka:any) {
     console.log(ka)
     this.router.navigateByUrl('search-results');
+  }
+
+  getUniqueKnowledgeArea(){
+    let uniqueKnowledgeAreasKeys = [...new Set(this.list.map((item:any) => item.design_group))];
+    uniqueKnowledgeAreasKeys.forEach((element:any) => {
+      let count  = this.list.filter((obj:any) => obj.design_group == element).length;
+      this.uniqueKnowledgeAreasWithCount.push({ key: element, value: count })
+    });
   }
 }
